80d5c9fe26150cc6b9437d8f4d770b712892fc77
Riemar <riemsjr534@gmail.com>
2026-05-26 10:52:47 +0800

    add

--- vite.config.js
+++ vite.config.js
@@ -9,16 +9,15 @@ export default defineConfig({
     babel({ presets: [reactCompilerPreset()] })
   ],
 
-  plugins: [react()],
   server: {
     host: "0.0.0.0",
-    port: 5173,
+    port: 5178,
     proxy: {
       "/api": {
-        target: "http://10.156.118.29:5000",
+        target: "http://127.0.0.1:5000",
         changeOrigin: true,
         secure: false,
-      },
-    },
+  },
+},
   },
 })
